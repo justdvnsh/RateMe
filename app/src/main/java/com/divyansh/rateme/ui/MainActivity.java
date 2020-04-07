8e766de10a3a8a364f11eef1c16e20a7259c9ea1
justdvnsh <justdvnsh2208@gmail.com>
2020-04-07 16:33:25 +0530

    added file

---
+++ app/src/main/java/com/divyansh/rateme/ui/MainActivity.java
@@ -0,0 +1,46 @@
+package com.divyansh.rateme.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.divyansh.rateme.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.saved_ratings) {
+            // do something
+        }
+
+        if (id == R.id.settings) {
+            // do something
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
